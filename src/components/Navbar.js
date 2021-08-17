109cbb0937395bbaccf69b8fa6a0f4cd1bd89072
oscar <antonespinoza@yahoo.com>
2021-08-17 17:16:18 +0000

    worked on navbar a litle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,36 @@
 import React from "react";
+import logo from "../img/Pentagram.png"
 
 const barStyles = {
-  height: "100%", /* 100% Full-height */
-  width: "7%", /* 0 width - change this with JavaScript */
-  position: "fixed", /* Stay in place */
-  zIndex: "1", /* Stay on top */
-  top: "0", /* Stay at the top */
-  backgroundColor: "hsl(169, 60%, 60%)", /* purple*/
-  opacity: "30%",
-  overflowX: "hidden", /* Disable horizontal scroll */
-  padding: "35% 1% 35% 1%"//padding for contents in the navbar
+    display:"flex",
+    verticalAlign:"center",
+    height: "7%", /* 100% Full-height */
+    width: "100%", /* 0 width - change this with JavaScript */
+    position: "fixed", /* Stay in place */
+    zIndex: "1", /* Stay on top */
+    top: "0", /* Stay at the top */
+    backgroundColor: "hsl(169, 60%, 60%)", /* purple*/
 }
 
 const barItem = {
-    paddingTop: "30%",//individual item padding within the navbar
-    paddingBottom: "30%"//individual adding within the navbar
+    paddingTop:"2.5%",
+    display:"inline",
+    paddingLeft:"13%",
+    paddingRight:"11%",
+    textDecoration:"none",
 }
+
 
 
 function Navbar(){
     return(
-        <div style = {barStyles} className = "navTag">
-            <h3 style = {barItem}>Home</h3>
-            <h3 style = {barItem}>Shop</h3>
-            <h3 style = {barItem}>About</h3>
+        <div>
+            <ul style = {barStyles}>
+                <li style = {barItem}><a href="home.html">home</a></li>
+                <li style = {barItem}><a href="shop.html">Shop</a></li>
+                <li style = {barItem}><a href="contact.html">Contact</a></li>
+                <li style = {barItem}><a href="customize.html">Customize</a></li>
+            </ul>
         </div>
     );
 }
